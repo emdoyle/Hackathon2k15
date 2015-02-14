c3ffd9f7592a6ca40ef5b697848ce739a24e6eb8
Evan Doyle <evanmdoyle@gmail.com>
2015-02-14 15:17:12 -0800

    Pushing Bag.java with charBag and bagDisplay arrays plus resetBag function

---
+++ Bag.java
@@ -0,0 +1,47 @@
+
+public class Bag {
+	private char[] charBag;
+	private boolean[] bagDisplay; //keeps track of which elems in bag should
+								//be displayed
+	
+	public Bag(char[] bag, boolean[] bagDisplay){
+		this.charBag = bag;
+		this.bagDisplay = bagDisplay;
+	}
+	
+	public void printBag(){
+		for(int i = 0; i < charBag.length; i++){
+			if(i != 0 && bagDisplay[i]){
+				System.out.print(" ");
+			}
+			if(bagDisplay[i]){
+				System.out.print(charBag[i]);
+			}
+			if(i == charBag.length - 1){
+				System.out.println("");
+			}
+		}
+	}
+	
+	public void resetBag(){
+		for(int i = 0; i < bagDisplay.length; i++){
+			bagDisplay[i] = true;
+		}
+	}
+	
+	public char[] getCharBag(){
+		return charBag;
+	}
+	
+	public void setCharBag(char[] newArr){
+		charBag = newArr;
+	}
+	
+	public boolean[] getBagDisplay(){
+		return bagDisplay;
+	}
+	
+	public void setBagDisplay(boolean[] newArr){
+		bagDisplay = newArr;
+	}
+}
