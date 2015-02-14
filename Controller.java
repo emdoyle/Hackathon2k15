902fa5bcae53d7397aed4f3feca8dd87afae4340
Jon Topielski <polkorame@gmail.com>
2015-02-14 15:15:20 -0800

    Created SubBoard.java class for other board

--- Controller.java
+++ Controller.java
@@ -1,10 +1,25 @@
 public class Controller {
 	public static void main(String[] args) {
-		Board board = new Board(13, 12, 3);
-		board.addBlock('+', 2);
-		board.addBlock('-', 1);
-		board.addBlock('-', 0);
-		board.evaluate();
+		SubBoard sb = new SubBoard(15, 0, 3);
+		SubBoard sb2 = new SubBoard(20, 0, 2);
+		sb.addBlock('+', 0);
+		sb.addBlock('-', 1);
+		sb.addBlock('+', 2);
+
+		sb2.addBlock('+', 0);
+		sb2.addBlock('+', 1);
+
+		int resOne = sb.evaluateResult();
+		int resTwo = sb2.evaluateResult();
+		int multBoth = resOne * resTwo;
+
+		Board board = new Board(multBoth, 30, 4);
+
+		sb.printBoard();
+		System.out.print("-------------------");
 		board.printBoard();
+		sb2.printBoard();
+
+
 	}
 }
---
+++ SubBoard.java
@@ -0,0 +1,46 @@
+/* Filename: SubBoard.java
+ * Description: Board for the display of the sub boards of the game that
+ *				are used to evaluate a sections of the board
+ */
+
+public class SubBoard extends Board {
+	// Block[] blocks;
+	// int startNum;
+	// int endNum;
+
+	/* Board constructor initializes an array of empty blocks into every space */
+	public SubBoard(int startNum, int endNum, int size) {
+		super(startNum, endNum, size);
+	}
+
+	/* displays the board on the command prompt */
+	public void printBoard() {
+		System.out.print("| " + startNum + " | ");
+		for (int i = 0; i < blocks.length; i++) {
+			System.out.print(blocks[i].getOperator() + " | ");
+		}
+		System.out.println();
+	}
+
+	/* getter for total */
+	public int getTotal() {
+		return endNum;
+	}
+
+
+	/* calls the evalOperator function and determines the outcome */
+	public int evaluateResult() {
+		if (!isFull()) {
+			System.err.println("Board not filled!");
+			return 0;
+		}
+
+		else {
+			endNum = startNum;
+			for (int i = 0; i < blocks.length; i++) {
+				endNum = evalOperator(endNum, 1, blocks[i].getOperator());
+			}
+		}
+		return endNum;
+	}
+}
